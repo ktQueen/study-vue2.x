aae0f5b7269969dba93dd82677532220e1e7fdb0
kangting <kangting@daojia-inc.com>
2020-04-09 15:12:02 +0800

    提交

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>index11</div>
+  <div>
+    {{count}}
+    {{this.$store.state.count}}
+    <button @click="submit">按钮</button>
+    <span @click="goList">列表页</span>
+  </div>
 </template>
 
 <script>
-
+import store from '../store'
 export default {
   name: 'index',
-  components: {
-    
+  store,
+  computed:{
+    count(){
+        return this.$store.state.count
+    }
+  },
+  methods:{
+    submit(){
+      store.commit('increment') 
+    },
+    goList(){
+      this.$router.push('/list')
+    }
   }
 }
 </script>
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>list</div>
+  <div>list
+    <p>{{msg}}</p>
+  </div>
 </template>
 
 <script>
-
+import store from '../store'
 export default {
   name: 'list',
+  store,
   components: {
     
+  },
+  data(){
+    return {
+      msg:store.state.count
+    }
   }
 }
 </script>
